4e9acb8144f9e2ae0a394748344f10c7044fc2fa
Hrvoje Niksic <hniksic@gmail.com>
2016-11-09 00:02:51 +0100

    Simpler error handling.

--- fsum.rs
+++ fsum.rs
@@ -2,33 +2,7 @@ use std::collections::{HashSet, VecDeque};
 use std::path::{PathBuf};
 use std::fs;
 use std::env;
-use std::io;
 use std::io::Write;
-use std::fmt;
-
-struct FileError {
-    err: io::Error,
-    filename: PathBuf,
-}
-
-impl fmt::Display for FileError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}: {}", self.filename.display(), self.err)
-    }
-}
-
-trait Contextify<T> {
-    fn imbue_err(self, filename: &PathBuf) -> Result<T, FileError>;
-}
-
-impl<T> Contextify<T> for Result<T, io::Error> {
-    fn imbue_err(self, filename: &PathBuf) -> Result<T, FileError> {
-        match self {
-            Err(err) => Err(FileError {err: err, filename: filename.clone()}),
-            Ok(v) => Ok(v),
-        }
-    }
-}
 
 fn fsum(args: &mut Iterator<Item=PathBuf>) -> u64
 {
@@ -37,19 +11,17 @@ fn fsum(args: &mut Iterator<Item=PathBuf>) -> u64
     let mut seen: HashSet<(u64, u64)> = HashSet::new();
     let mut total = 0u64;
 
-    fn log_error(e: FileError) {
-        writeln!(&mut std::io::stderr(), "{}", e).unwrap()
-    }
-
     while let Some(fl) = todo.pop_front() {
+        let log_error = |e| writeln!(std::io::stderr(), "{}: {}",
+                                     fl.display(), e).unwrap();
         (|| {
-            let mut meta = try!(fs::symlink_metadata(&fl).imbue_err(&fl));
+            let mut meta = try!(fs::symlink_metadata(&fl));
             if meta.file_type().is_symlink() {
                 let follow = fs::metadata(&fl);
                 if !follow.is_ok() {
                     return Ok(());  // don't log broken symlinks
                 }
-                meta = try!(follow.imbue_err(&fl));
+                meta = try!(follow);
             }
             let st = &meta as &std::os::unix::fs::MetadataExt;
             let file_id = (st.dev(), st.ino());
@@ -58,14 +30,14 @@ fn fsum(args: &mut Iterator<Item=PathBuf>) -> u64
             }
 
             if meta.is_dir() {
-                todo.extend(try!(fs::read_dir(&fl).imbue_err(&fl))
-                            .filter_map(|res| res.imbue_err(&fl).map_err(log_error).ok())
+                todo.extend(try!(fs::read_dir(&fl))
+                            .filter_map(|res| res.map_err(&log_error).ok())
                             .map(|dirent| dirent.path()));
             } else {
                 total += meta.len();
             }
             Ok(())
-        })().map_err(log_error).ok();
+        })().map_err(&log_error).ok();
     }
 
     total
