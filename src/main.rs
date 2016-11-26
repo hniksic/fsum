b565900d8124ae6fddb2bb68ddc9a739671f34f9
Hrvoje Niksic <hniksic@gmail.com>
2016-11-26 12:30:24 +0100

    Support terabytes.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main()
 {
     let size = fsum(&mut env::args_os().skip(1).map(PathBuf::from));
     println!("{}", size);
-    for &(power, digits, letter) in [(1<<10, 0, "K"), (1<<20, 2, "M"), (1<<30, 2, "G")].iter() {
+    for &(power, digits, letter) in [(1<<10, 0, "K"), (1<<20, 2, "M"), (1<<30, 2, "G"), (1<<40, 2, "T")].iter() {
         if size >= power {
             println!("{:.*} {}", digits, size as f64 / power as f64, letter)
         }
